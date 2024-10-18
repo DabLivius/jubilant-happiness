adc8669605795ca0c8841943b0c719923095d10e
MIAPTA <u20201186653@usco.edu.co>
2024-10-18 13:30:21 -0500

    Adicion de archivo .gitignore en el Backend para evitar el la subida del archivo .env

--- Backend/index.js
+++ Backend/index.js
@@ -53,3 +53,5 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
+//mongodb+srv://u20212200826:20212200826@cluster0.fcwil.mongodb.net/db?
